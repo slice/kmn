7bb39c2f78ca41ce01acff287535f95a8005b02e
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-08 13:51:25 -0700

    improve(cog): add ClientSession to every cog

--- kmn/cog.py
+++ kmn/cog.py
@@ -1,3 +1,17 @@
+from aiohttp import ClientSession
+
+
 class Cog:
     def __init__(self, bot):
         self.bot = bot
+        self.session = ClientSession(loop=bot.loop)
+
+        # not particularly proud of this
+        mangle_key = f'_{type(self).__name__}__unload'
+        self._original_unload = getattr(self, mangle_key, None)
+        setattr(self, mangle_key, self.__unload)
+
+    def __unload(self):
+        self.session.close()
+        if self._original_unload:
+            self._original_unload()
