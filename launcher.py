e09b9f500b7293f84861f31f25a7d82e1f351df2
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-07 22:14:59 -0700

    tweak(logging): proper logging setup

--- launcher.py
+++ launcher.py
@@ -4,7 +4,13 @@ import json
 from kmn.bot import Bot
 
 # setup logging
-logging.basicConfig(level=logging.INFO)
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+kmn_logger = logging.getLogger('kmn')
+kmn_logger.setLevel(logging.DEBUG)
+stream = logging.StreamHandler()
+stream.setFormatter(logging.Formatter('[{asctime}] [{levelname: <7}] {name}: {message}', '%Y-%m-%d %H:%M:%S', style='{'))
+root_logger.addHandler(stream)
 
 # read config
 with open('config.json', 'r') as fp:
