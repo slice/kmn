06aaf9a03e71e8db95f006682da087e461c8f049
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-07 22:15:20 -0700

    improve(formatting): add some neat helpers

--- kmn/formatting.py
+++ kmn/formatting.py
@@ -1,5 +1,21 @@
+from discord import Embed
+
+
 def codeblock(code, *, lang=''):
     return '```{}\n{}\n```'.format(lang, code)
+
+
+def describe(thing):
+    return f'{thing} (`{thing.id}`)'
+
+
+class KmnEmbed(Embed):
+    def add_fields(self, *fields, inline=False):
+        for name, value in fields:
+            self.add_field(name=name, value=value, inline=inline)
+
+    def add_blank_field(self):
+        self.add_field(name='\u200b', value='\u200b')
 
 
 def format_list(items, *, format, separator='\n'):
