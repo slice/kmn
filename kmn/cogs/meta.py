f3ac022d9a57d001bb1b4e3ae95edaeb5f95f312
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-11-05 21:11:57 -0800

    show server invite/source code link in k?about

--- kmn/cogs/meta.py
+++ kmn/cogs/meta.py
@@ -9,10 +9,19 @@ class Meta(Cog):
     @command(aliases=['info'], hidden=True)
     async def about(self, ctx):
         """about me!"""
+        server_invite = ctx.bot.config.get('server_invite')
+        source_code_link = ctx.bot.config.get('source_code_link')
+
         description = "i'm a cool bot" + (' (testing)' if ctx.bot.testing else '')
         embed = Embed(title=str(self.bot.user), color=Color.blurple(), description=description)
         admins = format_list(self.bot.config.get('admins', []), format='<@{item}>')
         embed.add_field(name='admins', value=admins)
+
+        if server_invite:
+            embed.add_field(name='server', value=server_invite)
+        if source_code_link:
+            embed.add_field(name='source', value=source_code_link)
+
         await ctx.send(embed=embed)
 
 
